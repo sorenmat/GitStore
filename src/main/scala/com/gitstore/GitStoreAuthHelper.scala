327902892032a06ebe005d22c343c2ebff27ab5e
Søren Mathiasen <sorenm@mymessages.dk>
2012-01-10 22:11:28 +0100

    new ldap model

--- src/main/scala/com/gitstore/GitStoreAuthHelper.scala
+++ src/main/scala/com/gitstore/GitStoreAuthHelper.scala
@@ -11,50 +11,62 @@ import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException
 import org.eclipse.jgit.lib.Repository
 import javax.servlet.http.HttpServletResponse
 import net.liftweb.common.Loggable
+import code.helpers.LDAPHelper
+import com.gitstore.auth.LDAPUtil
+import com.mongodb.BasicDBObject
 
 object GitStoreAuthHelper extends Loggable {
 	val BASIC = "Basic"
 	val CHALLENGE = "Basic" + " realm=\"Gitstore\""
 
 	def checkAuth(req: HttpServletRequest, resp: HttpServletResponse, db: Repository) {
-		val authorization = req.getHeader("Authorization")
-		if (authorization != null && authorization.startsWith(BASIC)) {
-			val base64Credentials = authorization.substring(BASIC.length()).trim()
-			val credentials = new String(Base64.decode(base64Credentials), Charset.forName("UTF-8"))
-			val values = credentials.split(":")
+		try {
 
-			if (values.length == 2) {
-				val username = values(0)
-				val password = values(1)
-				logger.info("user '" + username + "' tried to login")
-				val user = User.find(By(User.username, username), By(User.password, password))
-				user match {
-					case Full(u) => checkUserAccessToRepository(u, db)
-					case Empty => {
+			val authorization = req.getHeader("Authorization")
+			if (authorization != null && authorization.startsWith(BASIC)) {
+				val base64Credentials = authorization.substring(BASIC.length()).trim()
+				val credentials = new String(Base64.decode(base64Credentials), Charset.forName("UTF-8"))
+				val values = credentials.split(":")
+
+				if (values.length == 2) {
+					val username = values(0)
+					val password = values(1)
+					val authenticated = LDAPUtil.authenticate(username, password)
+					if (!authenticated) {
+						println("User '" + username + "' not authenticated !")
 						throw new ServiceNotAuthorizedException()
 					}
-					case Failure(_, _, _) =>
-						throw new ServiceNotAuthorizedException()
+					logger.info("user '" + username + "' tried to login")
+					val user = User.find(new BasicDBObject("username", username))
+					user match {
+						case Full(u) => checkUserAccessToRepository(u, db)
+						case Empty => {
+							val user = User.createRecord.username(username)
+							user.save
+							checkUserAccessToRepository(user, db)
+						}
+						case Failure(_, _, _) =>
+							throw new ServiceNotAuthorizedException()
+					}
 				}
+			} else {
+				resp.setHeader("WWW-Authenticate", CHALLENGE)
+				resp.sendError(HttpServletResponse.SC_UNAUTHORIZED)
 			}
-		} else {
-			resp.setHeader("WWW-Authenticate", CHALLENGE)
-			resp.sendError(HttpServletResponse.SC_UNAUTHORIZED)
-			throw new ServiceNotAuthorizedException()
+		} catch {
+			case e: Exception => e.printStackTrace(); throw new ServiceNotAuthorizedException()
 		}
 	}
 
 	def checkUserAccessToRepository(user: User, db: Repository) {
 		logger.debug("Direcotry name: " + db.getDirectory().getParentFile().getName())
 		val repositoryName = db.getDirectory().getParentFile().getName()
-		val repo = code.model.Repository.find(By(code.model.Repository.name, repositoryName))
-		repo match {
-			case Full(u) =>
-				if (u.users.find(repoUser => repoUser.username.toString == user.username.toString()).isEmpty) {
-					// is the repos groups all in the user group list
-					if (!u.groups.toSet.subsetOf(user.groups.toSet))
-						throw new ServiceNotAuthorizedException()
-				}
+		val repoOption = code.model.Repository.find(new BasicDBObject("name", repositoryName))
+		repoOption match {
+			case Full(repo) =>
+				val userGroups = LDAPUtil.getGroups(user.username.get).toSet
+				if (!repo.groups.get.toSet.subsetOf(userGroups))
+					throw new ServiceNotAuthorizedException()
 				logger.debug("User has access to repo")
 			case Empty => {
 				throw new ServiceNotAuthorizedException()
