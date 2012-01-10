4b83c9691f0c60014f43a59d63f3ff570e382405
Søren Mathiasen <sorenm@mymessages.dk>
2012-01-10 22:15:56 +0100

    Mongodb style

--- src/main/scala/code/helpers/DatabaseHelper.scala
+++ src/main/scala/code/helpers/DatabaseHelper.scala
@@ -1,44 +1,15 @@
 package code.helpers
-import com.gitstore.auth.GroupHelper
-import net.liftweb.common.Full
-import code.model.Group
-import net.liftweb.common.Empty
-import code.model.Repository
+import com.mongodb.DBObject
 import code.model.User
-import net.liftweb.mapper.By
-import net.liftweb.common.Failure
-import code.model.ServerSetup
+import com.mongodb.BasicDBObject
 
 object DatabaseHelper {
 	def init {
-
-		GroupHelper.getGroupAuthProvider.groups.foreach(grp => {
-			Group.find(By(Group.groupname, grp)) match {
-				case Full(x) =>
-				case Empty => {
-					println("Creating group: " + grp)
-					Group.create.groupname(grp).save
-				}
-				case Failure(_, _, _) =>
-			}
+		User.find(new BasicDBObject("username", "admin")).getOrElse({
+			val user = User.createRecord.username("admin").password("admin").isAdmin(true)
+			user.save
+			user
 		})
 
-		User.find(By(User.username, "admin")) match {
-			case Full(x) =>
-			case Empty => {
-				val admin = User.create.username("admin").password("admin").isAdmin(true)
-				val adminGroup = Group.find(By(Group.groupname, "admin")).getOrElse({
-					val grp = Group.create.groupname("admin")
-					grp.save
-					grp
-				})
-				admin.groups += adminGroup
-				admin.save
-			}
-			case Failure(_, _, _) =>
-		}
-
-		
-		LDAPHelper.sync
 	}
 }
